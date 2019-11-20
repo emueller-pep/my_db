e9076e1adec7069acc2e3eb4fbbcd9257c7ccf7d
Eric Mueller <eric.mueller@pepsico.com>
2019-11-20 15:41:37 -0500

    make the MyDb wrapper functional and tested

--- lib/my_db.ex
+++ lib/my_db.ex
@@ -1,18 +1,33 @@
 defmodule MyDb do
   @moduledoc """
-  Documentation for MyDb.
+  MyDb module (lib/my_db.ex) is intended to be an API module to a database. Imple-
+  ment its functions by forwarding the calls to the DbServer module. The API should
+  be as follows:
+
+  MyDb.start() → :ok
+  MyDb.stop() → :ok
+  MyDb.write(key, element) → :ok
+  MyDb.read(key) →{:ok, element} | {:error, :instance} MyDb.delete(key) → :ok
+  MyDb.match(element) → {:ok, [key1, ..., keyn]}
   """
 
-  @doc """
-  Hello world.
+  alias MyDb.DbServer, as: Server
 
-  ## Examples
+  @doc "Start the database server"
+  def start do Server.start end
 
-      iex> MyDb.hello()
-      :world
+  @doc "Stop the database server"
+  def stop do Server.stop() end
 
-  """
-  def hello do
-    :world
-  end
+  @doc "Write a key-value pair into the database"
+  def write(key, value) do Server.write(key, value) end
+
+  @doc "Delete a key-value pair from the database (given the key)"
+  def delete(key) do Server.delete(key) end
+
+  @doc "Read the value from the database for a given key"
+  def read(key) do Server.read(key) end
+
+  @doc "Find all keys that have a specified value in the database"
+  def match(value) do Server.match(value) end
 end
--- test/my_db_test.exs
+++ test/my_db_test.exs
@@ -2,7 +2,22 @@ defmodule MyDbTest do
   use ExUnit.Case
   doctest MyDb
 
-  test "greets the world" do
-    assert MyDb.hello() == :world
+  test "usage" do
+    assert MyDb.start == :ok
+    assert MyDb.write(:foo, 5) == :ok
+    assert MyDb.write(:bar, 6) == :ok
+    assert MyDb.write(:baz, 5) == :ok
+
+    assert MyDb.read(:foo) == { :ok, 5 }
+    assert MyDb.read(:blam) == { :error, :instance }
+
+    assert MyDb.match(5) == { :ok, [:baz, :foo] }
+    assert MyDb.match(6) == { :ok, [:bar] }
+    assert MyDb.match(7) == { :ok, [] }
+
+    assert MyDb.delete(:foo)
+    assert MyDb.read(:foo) == { :error, :instance }
+
+    assert MyDb.stop == :ok
   end
 end
